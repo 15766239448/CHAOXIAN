f81acce0466455c7ac01f64a2b8caef01f41f4db
15766239448 <15766239448@163.com>
2020-02-27 14:50:06 +0800

    申请表的dao和实体映射文件

--- src/main/java/cn/sz/gl/pojo/grshenqing.java
+++ src/main/java/cn/sz/gl/pojo/grshenqing.java
@@ -2,4 +2,74 @@ package cn.sz.gl.pojo;
 
 public class grshenqing {
 
+	private String name;
+	private Integer idcardnum;
+	private String picture;
+	private String address;
+	private String phone;
+	private String email;
+	private String gudingphone;
+	private String caogous;
+	private String gongyings;
+	private Integer id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getIdcardnum() {
+		return idcardnum;
+	}
+	public void setIdcardnum(Integer idcardnum) {
+		this.idcardnum = idcardnum;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGudingphone() {
+		return gudingphone;
+	}
+	public void setGudingphone(String gudingphone) {
+		this.gudingphone = gudingphone;
+	}
+	public String getCaogous() {
+		return caogous;
+	}
+	public void setCaogous(String caogous) {
+		this.caogous = caogous;
+	}
+	public String getGongyings() {
+		return gongyings;
+	}
+	public void setGongyings(String gongyings) {
+		this.gongyings = gongyings;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 }
--- src/main/java/cn/sz/gl/pojo/gsishenqing.java
+++ src/main/java/cn/sz/gl/pojo/gsishenqing.java
@@ -2,4 +2,88 @@ package cn.sz.gl.pojo;
 
 public class gsishenqing {
 
+	private String gsname;
+	private Integer id;
+	private String address;
+	private String xxaddress;
+	private String picture;
+	private String otherpicture;
+	private String faren ;
+	private String yunyname;
+	private String zhiwu;
+	private String yunyphone;
+	private String caigous;
+	private String gongyings;
+	public String getGsname() {
+		return gsname;
+	}
+	public void setGsname(String gsname) {
+		this.gsname = gsname;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getXxaddress() {
+		return xxaddress;
+	}
+	public void setXxaddress(String xxaddress) {
+		this.xxaddress = xxaddress;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public String getOtherpicture() {
+		return otherpicture;
+	}
+	public void setOtherpicture(String otherpicture) {
+		this.otherpicture = otherpicture;
+	}
+	public String getFaren() {
+		return faren;
+	}
+	public void setFaren(String faren) {
+		this.faren = faren;
+	}
+	public String getYunyname() {
+		return yunyname;
+	}
+	public void setYunyname(String yunyname) {
+		this.yunyname = yunyname;
+	}
+	public String getZhiwu() {
+		return zhiwu;
+	}
+	public void setZhiwu(String zhiwu) {
+		this.zhiwu = zhiwu;
+	}
+	public String getYunyphone() {
+		return yunyphone;
+	}
+	public void setYunyphone(String yunyphone) {
+		this.yunyphone = yunyphone;
+	}
+	public String getCaigous() {
+		return caigous;
+	}
+	public void setCaigous(String caigous) {
+		this.caigous = caigous;
+	}
+	public String getGongyings() {
+		return gongyings;
+	}
+	public void setGongyings(String gongyings) {
+		this.gongyings = gongyings;
+	}
 }
